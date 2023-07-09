abac2a853205a38ee6d9fdc3445d34bf4549b578
daisygith <daisygith@users.noreply.github.com>
2023-07-09 19:45:56 +0200

    create implementation app

--- src/main/java/dao/AppDAOImpl.java
+++ src/main/java/dao/AppDAOImpl.java
@@ -1,4 +1,18 @@
 package dao;
 
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class AppDAOImpl {
+
+    // define field for entity manager
+    private EntityManager entityManager;
+
+    @Autowired
+    public AppDAOImpl(EntityManager entityManager){
+        this.entityManager = entityManager;
+    }
+
 }
