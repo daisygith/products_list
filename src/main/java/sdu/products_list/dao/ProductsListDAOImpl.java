f3cdac866bd10f28acdc53242b76ec8fe070a176
daisygith <daisygith@users.noreply.github.com>
2023-07-11 20:57:46 +0200

    implements new method "findAllProducts"

--- src/main/java/sdu/products_list/dao/ProductsListDAOImpl.java
+++ src/main/java/sdu/products_list/dao/ProductsListDAOImpl.java
@@ -1,11 +1,15 @@
 package sdu.products_list.dao;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import sdu.products_list.entity.ProductsList;
+
+import java.util.List;
 
 @Repository
-public class ProductsListDAOImpl {
+public class ProductsListDAOImpl implements ProductsListDAO{
 
     // define field for entity manager
     private EntityManager entityManager;
@@ -15,4 +19,15 @@ public class ProductsListDAOImpl {
         this.entityManager = entityManager;
     }
 
+    @Override
+    public List<ProductsList> findAllProducts() {
+
+        TypedQuery<ProductsList> query = entityManager.createQuery(
+                "from ProductsList", ProductsList.class);
+
+
+        List<ProductsList> productslist = query.getResultList();
+
+        return productslist;
+    }
 }
