ee1cf0f10e9818b42961aa1eef0b14443b8f0f66
daisygith <daisygith@users.noreply.github.com>
2023-07-11 19:52:40 +0200

    Add name of columns form database "products_list" generated constructors, getter/setter and toString()

--- src/main/java/sdu/products_list/entity/ProductsList.java
+++ src/main/java/sdu/products_list/entity/ProductsList.java
@@ -1,4 +1,61 @@
 package sdu.products_list.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="products_list")
 public class ProductsList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="unit")
+    private String unit;
+
+    public ProductsList(String name, String unit) {
+        this.name = name;
+        this.unit = unit;
+    }
+
+    public ProductsList(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductsList{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", unit='" + unit + '\'' +
+                '}';
+    }
 }
