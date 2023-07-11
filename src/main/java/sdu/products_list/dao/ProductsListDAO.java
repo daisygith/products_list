b7d7507c55665de7391fd0ffab4046e6ddce4a38
daisygith <daisygith@users.noreply.github.com>
2023-07-11 20:57:23 +0200

    add new method "findAllProducts"

--- src/main/java/sdu/products_list/dao/ProductsListDAO.java
+++ src/main/java/sdu/products_list/dao/ProductsListDAO.java
@@ -1,4 +1,10 @@
 package sdu.products_list.dao;
 
+import sdu.products_list.entity.ProductsList;
+
+import java.util.List;
+
 public interface ProductsListDAO {
+
+    List<ProductsList> findAllProducts();
 }
