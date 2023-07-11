16a9a273e000b85c3e53a01182f08596f0e531a9
daisygith <daisygith@users.noreply.github.com>
2023-07-11 20:58:56 +0200

    add RestController and new GetMapping

--- src/main/java/sdu/products_list/rest/ProductsListRestController.java
+++ src/main/java/sdu/products_list/rest/ProductsListRestController.java
@@ -1,4 +1,30 @@
 package sdu.products_list.rest;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sdu.products_list.entity.ProductsList;
+import sdu.products_list.service.ProductsListService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class ProductsListRestController {
+
+    @Autowired
+    private ProductsListService productsListService;
+//
+//    public ProductsListRestController(ProductsListService theProductsListService){
+//        productsListService = theProductsListService;
+//    }
+
+    @GetMapping("/productslist")
+    public List<ProductsList> findAllProducts(){
+        return productsListService.findAllProducts();
+    }
+
+
 }
