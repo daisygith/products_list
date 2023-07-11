737ea0a4595202adfdee684dec74927991ebfbef
daisygith <daisygith@users.noreply.github.com>
2023-07-11 20:59:44 +0200

    implement new method findAllProducts

--- src/main/java/sdu/products_list/service/ProductsListServiceImpl.java
+++ src/main/java/sdu/products_list/service/ProductsListServiceImpl.java
@@ -1,4 +1,25 @@
 package sdu.products_list.service;
 
-public class ProductsListServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import sdu.products_list.dao.ProductsListDAO;
+import sdu.products_list.entity.ProductsList;
+
+import java.util.List;
+
+@Service
+public class ProductsListServiceImpl implements ProductsListService{
+
+    @Autowired
+    private ProductsListDAO productsListDAO;
+
+//    @Autowired
+//    public ProductsListServiceImpl(ProductsListDAO theProductsListDAO){
+//        productsListDAO = theProductsListDAO;
+//    }
+
+    @Override
+    public List<ProductsList> findAllProducts(){
+        return productsListDAO.findAllProducts();
+    }
 }
