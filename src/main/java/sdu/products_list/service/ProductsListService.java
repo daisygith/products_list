ea81851a28049a5cfe85f105eca5b48a4dea093a
daisygith <daisygith@users.noreply.github.com>
2023-07-11 20:59:21 +0200

    add new method findAllProducts

--- src/main/java/sdu/products_list/service/ProductsListService.java
+++ src/main/java/sdu/products_list/service/ProductsListService.java
@@ -1,4 +1,11 @@
 package sdu.products_list.service;
 
+import sdu.products_list.entity.ProductsList;
+
+import java.util.List;
+
 public interface ProductsListService {
+
+    List<ProductsList> findAllProducts();
+
 }
