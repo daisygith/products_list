d412a0b012ccc361d1fe03d2f40cb80e34408cfe
daisygith <daisygith@users.noreply.github.com>
2023-07-17 18:26:29 +0200

    create new method - find all recipes at the list

--- src/main/java/sdu/products_list/dao/RecipesListDAO.java
+++ src/main/java/sdu/products_list/dao/RecipesListDAO.java
@@ -1,4 +1,11 @@
 package sdu.products_list.dao;
 
+import sdu.products_list.entity.RecipesList;
+
+import java.util.List;
+
 public interface RecipesListDAO {
+
+    List<RecipesList> findAll();
+
 }
