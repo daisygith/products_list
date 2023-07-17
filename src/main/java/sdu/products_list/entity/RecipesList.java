ff0f52f057479206b7d88c5df31f616470b83505
daisygith <daisygith@users.noreply.github.com>
2023-07-17 17:36:15 +0200

    create table which will be connect with database

--- src/main/java/sdu/products_list/entity/RecipesList.java
+++ src/main/java/sdu/products_list/entity/RecipesList.java
@@ -1,4 +1,48 @@
 package sdu.products_list.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "recipes_list")
 public class RecipesList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    public RecipesList(String name) {
+        this.name = name;
+    }
+
+    public RecipesList() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "RecipesList{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
