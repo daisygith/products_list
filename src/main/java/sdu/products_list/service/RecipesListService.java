24168c4375dfa1398c128838d9a13f826c2458a5
daisygith <daisygith@users.noreply.github.com>
2023-07-17 18:46:29 +0200

    create method findAll()

--- src/main/java/sdu/products_list/service/RecipesListService.java
+++ src/main/java/sdu/products_list/service/RecipesListService.java
@@ -1,4 +1,9 @@
 package sdu.products_list.service;
 
+import sdu.products_list.entity.RecipesList;
+
+import java.util.List;
+
 public interface RecipesListService {
+    List<RecipesList> findAll();
 }
