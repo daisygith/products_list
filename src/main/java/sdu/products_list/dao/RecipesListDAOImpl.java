ed4b1ca40bd14fcf7c5b1502010a615982c0d06b
daisygith <daisygith@users.noreply.github.com>
2023-07-17 18:26:39 +0200

    implement new method - find all recipes at the list

--- src/main/java/sdu/products_list/dao/RecipesListDAOImpl.java
+++ src/main/java/sdu/products_list/dao/RecipesListDAOImpl.java
@@ -1,4 +1,34 @@
 package sdu.products_list.dao;
 
-public class RecipesListDAOImpl {
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import sdu.products_list.entity.RecipesList;
+
+import java.util.List;
+
+public class RecipesListDAOImpl implements RecipesListDAO{
+
+    //define fields for entitymanager
+    private EntityManager entityManager;
+
+    // set up constructor injuction
+    @Autowired
+    public RecipesListDAOImpl(EntityManager theEntityManager){
+        entityManager = theEntityManager;
+    }
+
+    @Override
+    public List<RecipesList> findAll() {
+
+        // create a query
+        TypedQuery<RecipesList> theQuery = entityManager.createQuery(
+                "from RecipesList", RecipesList.class);
+
+        //execute query and get result list
+        List<RecipesList> recipesLists = theQuery.getResultList();
+
+        //return the results
+        return recipesLists;
+    }
 }
