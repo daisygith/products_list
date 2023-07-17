d0f4ef7063a70b1edddee5afe51d1a41428113d5
daisygith <daisygith@users.noreply.github.com>
2023-07-17 18:49:11 +0200

    create to connect with database and add new GetMapping

--- src/main/java/sdu/products_list/rest/RecipesListRestController.java
+++ src/main/java/sdu/products_list/rest/RecipesListRestController.java
@@ -1,4 +1,30 @@
 package sdu.products_list.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sdu.products_list.entity.RecipesList;
+import sdu.products_list.service.ProductsListService;
+import sdu.products_list.service.RecipesListService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/rec")
 public class RecipesListRestController {
+
+    private RecipesListService recipesListService;
+
+    // quick and dirty: injection employee dao(use constructor injection)
+    public RecipesListRestController(RecipesListService theRecipesListService) {
+        recipesListService = theRecipesListService;
+    }
+
+    // expose "/recipeslist" and return a list of employee
+    @GetMapping("/recipeslist")
+    public List<RecipesList> findAll(){
+
+        return recipesListService.findAll();
+        }
+
 }
