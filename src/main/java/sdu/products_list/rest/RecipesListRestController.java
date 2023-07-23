5f3d54038386f7b8603d967237eca20dbd9756b1
daisygith <daisygith@users.noreply.github.com>
2023-07-23 14:09:03 +0200

    implement method to find recipe by Id

--- src/main/java/sdu/products_list/rest/RecipesListRestController.java
+++ src/main/java/sdu/products_list/rest/RecipesListRestController.java
@@ -1,6 +1,7 @@
 package sdu.products_list.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sdu.products_list.entity.RecipesList;
@@ -26,5 +27,18 @@ public class RecipesListRestController {
 
         return recipesListService.findAll();
         }
+
+        // add mapping for GET /recipeslist/{recipeID}
+    @GetMapping("recipeslist/{recipeId}")
+    public RecipesList getRecipesList(@PathVariable int recipeId){
+
+        RecipesList theRecipe = recipesListService.findById(recipeId);
+
+        if(theRecipe == null){
+            throw new RuntimeException("Recipe id not found - " + recipeId);
+        }
+        return theRecipe;
+    }
+
 
 }
