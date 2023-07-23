4c040eeae997402514a5ec845a9d1765554ae9b7
daisygith <daisygith@users.noreply.github.com>
2023-07-23 18:29:38 +0200

    create table from database for shop list

--- src/main/java/sdu/products_list/entity/ShopList.java
+++ src/main/java/sdu/products_list/entity/ShopList.java
@@ -1,4 +1,55 @@
 package sdu.products_list.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "shop_list")
 public class ShopList {
+
+    // dodanie kolumn
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY) // primary key
+    @Column(name= "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    public ShopList(String name) {
+        this.name = name;
+    }
+
+    // konstructor wszystko bez id
+    public ShopList(){
+
+    }
+
+    // getter setter - dla wszystkich zmiennych nawet id
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // toString() także ze wszystkimi zmiennymi
+
+    @Override
+    public String toString() {
+        return "ShopList{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
