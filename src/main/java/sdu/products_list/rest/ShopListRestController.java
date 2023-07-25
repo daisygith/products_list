f3d5ae9a655780eb347b3c730e46acb6d5ceb6a8
daisygith <daisygith@users.noreply.github.com>
2023-07-25 19:53:08 +0200

    create some crud implement

--- src/main/java/sdu/products_list/rest/ShopListRestController.java
+++ src/main/java/sdu/products_list/rest/ShopListRestController.java
@@ -1,4 +1,46 @@
 package sdu.products_list.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import sdu.products_list.entity.ShopList;
+import sdu.products_list.service.ShopListService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/shop")
 public class ShopListRestController {
+
+    @Autowired
+    private ShopListService shopListService;
+
+    @GetMapping("/shoplist")
+    public List<ShopList> findAll(){
+        return shopListService.findAll();
+    }
+
+    @GetMapping("/shoplist/{shopListId}")
+    public ShopList getShopList(@PathVariable int shopListId){
+
+        ShopList theShopList = shopListService.findById(shopListId);
+
+        if(theShopList == null){
+            throw new RuntimeException("The product is not found - " + shopListId);
+        }
+
+        return theShopList;
+
+    }
+
+    @PostMapping("/shoplist")
+    public ShopList addShopList(@RequestBody ShopList theShopList){
+
+        theShopList.setId(0);
+
+        ShopList dbShopList = shopListService.save(theShopList);
+
+        return dbShopList;
+    }
+
+
 }
