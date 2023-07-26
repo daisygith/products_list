a83da87f7ee260cbaa99e9d2178fe162978133d9
daisygith <daisygith@users.noreply.github.com>
2023-07-26 18:28:05 +0200

    add new annotation, and can delete constructor, getter and setter and toString

--- src/main/java/sdu/products_list/entity/ShopList.java
+++ src/main/java/sdu/products_list/entity/ShopList.java
@@ -1,7 +1,15 @@
 package sdu.products_list.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "shop_list")
 public class ShopList {
@@ -15,41 +23,41 @@ public class ShopList {
     @Column(name = "name")
     private String name;
 
-    public ShopList(String name) {
-        this.name = name;
-    }
-
-    // konstructor wszystko bez id
-    public ShopList(){
-
-    }
-
-    // getter setter - dla wszystkich zmiennych nawet id
-
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    // toString() także ze wszystkimi zmiennymi
-
-    @Override
-    public String toString() {
-        return "ShopList{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
+//    public ShopList(String name) {
+//        this.name = name;
+//    }
+//
+//    // konstructor wszystko bez id
+//    public ShopList(){
+//
+//    }
+//
+//    // getter setter - dla wszystkich zmiennych nawet id
+//
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    // toString() także ze wszystkimi zmiennymi
+//
+//    @Override
+//    public String toString() {
+//        return "ShopList{" +
+//                "id=" + id +
+//                ", name='" + name + '\'' +
+//                '}';
+//    }
 }
