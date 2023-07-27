e85437778e4be9263167e24ae8bece5f869ad31f
daisygith <daisygith@users.noreply.github.com>
2023-07-27 20:16:08 +0200

    new implementation for dto

--- src/main/java/sdu/products_list/service/ShopListServiceImpl.java
+++ src/main/java/sdu/products_list/service/ShopListServiceImpl.java
@@ -1,12 +1,15 @@
 package sdu.products_list.service;
 
+import org.apache.logging.log4j.CloseableThreadContext;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 import sdu.products_list.dao.ShopListDAO;
+import sdu.products_list.dto.ShopListDTO;
 import sdu.products_list.entity.ShopList;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -15,27 +18,49 @@ public class ShopListServiceImpl implements ShopListService{
     @Autowired
     private ShopListDAO shopListDAO;
 
-    @Autowired
-    public ShopListServiceImpl(ShopListDAO theShopListDAO){
-        shopListDAO = theShopListDAO;
+    @Override
+    public List<ShopListDTO> findAll() {
+        // lista z wszystkimi elementami bazy danych
+       List<ShopList> list = shopListDAO.findAllShopList();
+       // utworzenie pustej listy DTO
+       List<ShopListDTO> listDTO = new ArrayList<>();
+        // przepisanie wartości z encji do obiektów dto
+       list.forEach((ShopList item) -> {
+           listDTO.add(new ShopListDTO(item.getId(),item.getName()));
+       });
+        // zwracanie listy DTO
+       return listDTO;
     }
-
 
     @Override
-    public List<ShopList> findAll() {
-        return shopListDAO.findAllShopList();
+    public ShopListDTO findById(int theId) {
+
+        ShopList product = shopListDAO.findById(theId);
+
+        ShopListDTO productDTO = new ShopListDTO(product.getId(), product.getName());
+
+        return productDTO;
+
     }
 
-    @Override
-    public ShopList findById(int theId) {
-
-        return shopListDAO.findById(theId);
-    }
+//    @Override
+//    public ShopListDTO save(ShopListDTO theShopList) {
+//        return null;
+//    }
 
     @Transactional
     @Override
-    public ShopList save(ShopList theShopList) {
-        return shopListDAO.save(theShopList);
+    public ShopListDTO save(ShopListDTO theShopListDTO) {
+        // dla merge
+        ShopList product = shopListDAO.save(new ShopList(theShopListDTO.getId(), theShopListDTO.getName()));
+        // dla persist + trzebaby było rozbić na save i update
+        //ShopList product = new ShopList(theShopListDTO.getId(), theShopListDTO.getName());
+        //shopListDAO.save(product);
+
+        ShopListDTO productDTO = new ShopListDTO(product.getId(), product.getName());
+
+        return productDTO;
+        //return shopListDAO.save(theShopList);
     }
 
     @Transactional
