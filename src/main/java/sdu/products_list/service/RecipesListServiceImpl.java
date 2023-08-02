e56c24d67c3e6c0a4a89bfeaa774d1145252df96
daisygith <daisygith@users.noreply.github.com>
2023-08-02 19:55:10 +0200

    implementation to DTO

--- src/main/java/sdu/products_list/service/RecipesListServiceImpl.java
+++ src/main/java/sdu/products_list/service/RecipesListServiceImpl.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import sdu.products_list.dao.RecipesListDAO;
+import sdu.products_list.dto.RecipesListDTO;
 import sdu.products_list.entity.RecipesList;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -14,35 +16,48 @@ public class RecipesListServiceImpl implements RecipesListService {
     @Autowired
     private RecipesListDAO recipesListDAO;
 
-    @Autowired
-    public RecipesListServiceImpl(RecipesListDAO theRecipesListDAO){
-        recipesListDAO = theRecipesListDAO;
+    @Override
+    public List<RecipesListDTO> findAll() {
+        // pobieramy liste ze wszystkimi elementami baz danych
+        List<RecipesList> recipeList = recipesListDAO.findAll();
+        //utworzenie pustej listy DTO
+        List<RecipesListDTO> recipeListDTO = new ArrayList<>();
+        //przepisanie wartosci z encji do DTO
+        recipeList.forEach((RecipesList item) -> {
+            recipeListDTO.add(new RecipesListDTO(item.getId(), item.getName()));
+        });
+
+        return recipeListDTO;
+
     }
 
     @Override
-    public List<RecipesList> findAll() {
-        return recipesListDAO.findAll();
+    public RecipesListDTO findById(int theId) {
 
-    }
+        RecipesList productRecipe = recipesListDAO.findById(theId);
 
-    @Override
-    public RecipesList findById(int theId) {
+        RecipesListDTO productRecipeDTO = new RecipesListDTO(productRecipe.getId(), productRecipe.getName());
 
-        return recipesListDAO.findById(theId);
+        return productRecipeDTO;
 
     }
 
     @Transactional
     @Override
-    public RecipesList save(RecipesList theRecipe) {
+    public RecipesListDTO save(RecipesListDTO theRecipeDTO) {
 
-        return recipesListDAO.save(theRecipe);
+        RecipesList productRecipe = recipesListDAO.save(new RecipesList(theRecipeDTO.getId(), theRecipeDTO.getName()));
+
+        RecipesListDTO productRecipeDTO = new RecipesListDTO(productRecipe.getId(), productRecipe.getName());
+
+        return productRecipeDTO;
 
     }
 
     @Transactional
     @Override
     public void deleteById(int theId) {
+
         recipesListDAO.deleteById(theId);
     }
 }
