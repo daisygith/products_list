04a525abf4df3d2d670493387b1671373122db0b
daisygith <daisygith@users.noreply.github.com>
2023-08-03 18:15:30 +0200

    create entity to StepList

--- src/main/java/sdu/products_list/entity/StepList.java
+++ src/main/java/sdu/products_list/entity/StepList.java
@@ -1,4 +1,28 @@
 package sdu.products_list.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "step_list")
 public class StepList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "step_nr")
+    private int stepNr;
+
+    @Column(name = "description")
+    private String description;
+
 }
