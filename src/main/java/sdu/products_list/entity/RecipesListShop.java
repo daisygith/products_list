b9fdfa14652456477eac5a09088c484786956382
daisygith <daisygith@users.noreply.github.com>
2023-08-03 19:34:28 +0200

    starting create RecipesListShop

--- src/main/java/sdu/products_list/entity/RecipesListShop.java
+++ src/main/java/sdu/products_list/entity/RecipesListShop.java
@@ -1,4 +1,14 @@
 package sdu.products_list.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class RecipesListShop {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+
 }
