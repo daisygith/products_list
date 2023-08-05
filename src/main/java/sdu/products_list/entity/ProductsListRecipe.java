edb89ec8603e3099405b1a15745c93f434bfdb66
daisygith <daisygith@users.noreply.github.com>
2023-08-05 11:36:28 +0200

    create products_list_recipe

--- src/main/java/sdu/products_list/entity/ProductsListRecipe.java
+++ src/main/java/sdu/products_list/entity/ProductsListRecipe.java
@@ -1,4 +1,25 @@
 package sdu.products_list.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="products_list_recipe")
 public class ProductsListRecipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "qty")
+    private float gty;
+
 }
