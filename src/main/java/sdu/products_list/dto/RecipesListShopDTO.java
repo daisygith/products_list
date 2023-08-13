43be8aa9af1990c8a338223fbdfbe0b9ed154bb1
daisygith <daisygith@users.noreply.github.com>
2023-08-13 19:13:50 +0200

    add class RecipesListDTO

---
+++ src/main/java/sdu/products_list/dto/RecipesListShopDTO.java
@@ -0,0 +1,18 @@
+package sdu.products_list.dto;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import sdu.products_list.entity.RecipesList;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class RecipesListShopDTO {
+
+    private int id;
+    private RecipesListDTO recipesList;
+
+}
