9c5695be1df3005a9eec0eb637486e05cb31d5a0
daisygith <daisygith@users.noreply.github.com>
2023-08-30 21:49:05 +0200

    some comments method - need to step-by-step testing

--- src/main/java/sdu/products_list/rest/ProductsListRestController.java
+++ src/main/java/sdu/products_list/rest/ProductsListRestController.java
@@ -22,48 +22,48 @@ public class ProductsListRestController {
         return productsListService.findAllProducts();
     }
 
-    @GetMapping("/productslist/{productslistId}")
-    public ProductsListDTO getProductsList(@PathVariable int productslistId){
-        ProductsListDTO theProductsList = productsListService.findById(productslistId);
-
-        if(theProductsList == null){
-            throw new RuntimeException("Products List id not found" + productslistId);
-        }
-        return theProductsList;
-
-    }
-
-    @PostMapping("/productslist")
-    public ProductsListDTO addProducts(@RequestBody ProductsListDTO theProductsDTO){
-
-        theProductsDTO.setId(0);
-
-        ProductsListDTO dbProducts = productsListService.save(theProductsDTO);
-
-        return dbProducts;
-    }
-
-    @PutMapping("/productslist")
-    public ProductsListDTO updateProducts(@RequestBody ProductsListDTO theProductsDTO){
-
-        ProductsListDTO dbProducts = productsListService.save(theProductsDTO);
-
-        return dbProducts;
-
-    }
-
-    @DeleteMapping("/productslist/{productslistId}")
-    public String deleteProducts(@PathVariable int productslistId){
-
-        ProductsListDTO tempProducts = productsListService.findById(productslistId);
-
-        if(tempProducts == null){
-            throw new RuntimeException("Product is not found - " + productslistId);
-        }
-
-        productsListService.deleteById(productslistId);
-
-        return "Delete products id - " + productslistId;
-    }
+//    @GetMapping("/productslist/{productslistId}")
+//    public ProductsListDTO getProductsList(@PathVariable int productslistId){
+//        ProductsListDTO theProductsList = productsListService.findById(productslistId);
+//
+//        if(theProductsList == null){
+//            throw new RuntimeException("Products List id not found" + productslistId);
+//        }
+//        return theProductsList;
+//
+//    }
+//
+//    @PostMapping("/productslist")
+//    public ProductsListDTO addProducts(@RequestBody ProductsListDTO theProductsDTO){
+//
+//        theProductsDTO.setId(0);
+//
+//        ProductsListDTO dbProducts = productsListService.save(theProductsDTO);
+//
+//        return dbProducts;
+//    }
+//
+//    @PutMapping("/productslist")
+//    public ProductsListDTO updateProducts(@RequestBody ProductsListDTO theProductsDTO){
+//
+//        ProductsListDTO dbProducts = productsListService.save(theProductsDTO);
+//
+//        return dbProducts;
+//
+//    }
+//
+//    @DeleteMapping("/productslist/{productslistId}")
+//    public String deleteProducts(@PathVariable int productslistId){
+//
+//        ProductsListDTO tempProducts = productsListService.findById(productslistId);
+//
+//        if(tempProducts == null){
+//            throw new RuntimeException("Product is not found - " + productslistId);
+//        }
+//
+//        productsListService.deleteById(productslistId);
+//
+//        return "Delete products id - " + productslistId;
+//    }
 
 }
