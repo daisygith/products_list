ff1004402146246f120c48bc0a9d262a14bfa9c7
daisygith <daisygith@users.noreply.github.com>
2023-09-01 09:31:52 +0200

    change method findById - adding a method call ( productsListRecipe

--- src/main/java/sdu/products_list/service/ProductsListServiceImpl.java
+++ src/main/java/sdu/products_list/service/ProductsListServiceImpl.java
@@ -5,10 +5,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import sdu.products_list.dao.ProductsListDAO;
 import sdu.products_list.dto.ProductsListDTO;
+import sdu.products_list.dto.ProductsListRecipeDTO;
 import sdu.products_list.entity.ProductsList;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductsListServiceImpl implements ProductsListService{
@@ -34,17 +36,27 @@ public class ProductsListServiceImpl implements ProductsListService{
 
         return productsListDTO;
     }
-//
-//    @Override
-//    public ProductsListDTO findById(int theId) {
-//
-//        ProductsList productList = productsListDAO.findById(theId);
-//
-//        ProductsListDTO productListDTO = new ProductsListDTO(productList.getId(), productList.getName(), productList.getUnit());
-//
-//        return productListDTO;
-//    }
-//
+
+    @Override
+    public ProductsListDTO findById(int theId) {
+
+        ProductsList productList = productsListDAO.findById(theId);
+
+        //ProductsListDTO productListDTO = new ProductsListDTO(productList.getId(), productList.getName(), productList.getUnit());
+        ProductsListDTO productListDTO = ProductsListDTO.builder()
+                .id(productList.getId())
+                .name(productList.getName())
+                .unit(productList.getUnit())
+                .productsListRecipe(productList.getProductsListRecipes().stream()
+                        .map(x-> ProductsListRecipeDTO.builder()
+                                .id(x.getId())
+                                .qty(x.getQty())
+                                .build()).collect(Collectors.toList()))
+                .build();
+
+        return productListDTO;
+    }
+
 //    @Transactional
 //    @Override
 //    public ProductsListDTO save(ProductsListDTO theProductsDTO) {
