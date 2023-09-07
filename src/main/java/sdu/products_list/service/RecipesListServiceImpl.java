1e53780a09c501d7061bcf58d53ef565cc9a7e81
daisygith <daisygith@users.noreply.github.com>
2023-09-07 19:07:22 +0200

    add .productsList elements, and change method call

--- src/main/java/sdu/products_list/service/RecipesListServiceImpl.java
+++ src/main/java/sdu/products_list/service/RecipesListServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import sdu.products_list.dto.*;
+import sdu.products_list.entity.ProductsList;
 import sdu.products_list.entity.ProductsListRecipe;
 import sdu.products_list.entity.RecipesList;
 import sdu.products_list.dao.RecipesListDAO;
@@ -22,17 +23,17 @@ public class RecipesListServiceImpl implements RecipesListService {
 
     @Override
     public List<RecipesListDTO> findAll() {
-        // pobieramy liste ze wszystkimi elementami baz danych
+
         List<RecipesList> recipeList = recipesListDAO.findAll();
-        //utworzenie pustej listy DTO
+
         List<RecipesListDTO> recipeListDTO = new ArrayList<>();
-        //przepisanie wartosci z encji do DTO
-            recipeList.forEach((RecipesList item) -> {
-                recipeListDTO.add(RecipesListDTO.builder()
-                        .id(item.getId())
-                        .name(item.getName())
-                        .build());
-            });
+
+        recipeList.forEach((RecipesList item) -> {
+            recipeListDTO.add(RecipesListDTO.builder()
+                    .id(item.getId())
+                    .name(item.getName())
+                    .build());
+        });
 
         return recipeListDTO;
 
@@ -43,26 +44,28 @@ public class RecipesListServiceImpl implements RecipesListService {
 
         RecipesList productRecipe = recipesListDAO.findById(theId);
 
-//        productRecipe.getRecipeListShops();
-        //many-to-many
         productRecipe.getShopList();
 
         RecipesListDTO productRecipeDTO = RecipesListDTO.builder()
                 .id(productRecipe.getId())
                 .name(productRecipe.getName())
                 .stepList(productRecipe.getStepList().stream()
-                        .map(x->StepListDTO.builder()
-                        .id(x.getId()).stepNr(x.getStepNr()).description(x.getDescription())
+                        .map(x -> StepListDTO.builder()
+                                .id(x.getId()).stepNr(x.getStepNr()).description(x.getDescription())
                                 .build()).collect(Collectors.toList()))
-                .shopListSet(productRecipe.getShopList().stream()
-                        .map(x-> ShopListDTO.builder()
-                                .id(x.getId())
-                                .name(x.getName())
-                                .build()).collect(Collectors.toList()))
+//                .shopListSet(productRecipe.getShopList().stream()
+//                        .map(x-> ShopListDTO.builder()
+//                                .id(x.getId())
+//                                .name(x.getName())
+//                                .build()).collect(Collectors.toList()))
                 .productsListRecipe(productRecipe.getProductsListRecipes().stream()
-                        .map(x-> ProductsListRecipeDTO.builder()
-                                .id(x.getId()).qty(x.getQty())
-                        .build()).collect(Collectors.toList()))
+                        .map(x -> ProductsListRecipeDTO.builder()
+                                .id(x.getId())
+                                .qty(x.getQty())
+                                .productsListId(x.getProductsList().getId())
+                                .productsListName(x.getProductsList().getName())
+                                .productsListUnit(x.getProductsList().getUnit())
+                                .build()).collect(Collectors.toList()))
                 .build();
 
         return productRecipeDTO;
@@ -73,22 +76,32 @@ public class RecipesListServiceImpl implements RecipesListService {
     @Override
     public RecipesListDTO save(RecipesListDTO theRecipeDTO) {
 
-        RecipesList productRecipe = recipesListDAO.save(RecipesList.builder()
-                        .id(theRecipeDTO.getId())
-                        .name(theRecipeDTO.getName())
-                        .stepList(theRecipeDTO.getStepList() != null ? theRecipeDTO.getStepList().stream()
-                                .map(x-> StepList.builder()
-                                        .id(x.getId()).stepNr(x.getStepNr()).description(x.getDescription())
-                                .build()).collect(Collectors.toList()) : null)
-//                        .recipesListShops(theRecipeDTO.getRecipesListShop() != null ? theRecipeDTO.getRecipesListShop().stream()
-//                                .map(x-> RecipesListShop.builder()
-//                                        .id(x.getId())
-//                                        .build()).collect(Collectors.toList()) : null)
-                        .productsListRecipes(theRecipeDTO.getProductsListRecipe() != null ? theRecipeDTO.getProductsListRecipe().stream()
-                                .map(x-> ProductsListRecipe.builder()
-                                        .id(x.getId()).qty(x.getQty())
-                                        .build()).collect(Collectors.toList()) : null)
-                        .build());
+        RecipesList productRecipe1 = RecipesList.builder()
+                .id(theRecipeDTO.getId())
+                .name(theRecipeDTO.getName())
+                .build();
+
+        productRecipe1.setStepList(theRecipeDTO.getStepList() != null ? theRecipeDTO.getStepList().stream()
+                .map(x -> StepList.builder()
+                        .id(x.getId())
+                        .stepNr(x.getStepNr())
+                        .description(x.getDescription())
+                        .recipesList(productRecipe1)
+                        .build())
+                .collect(Collectors.toList()) : null);
+        productRecipe1.setProductsListRecipes(theRecipeDTO.getProductsListRecipe() != null ? theRecipeDTO.getProductsListRecipe().stream()
+                .map(x -> ProductsListRecipe.builder()
+                        .id(x.getId())
+                        .qty(x.getQty())
+                        .recipesList(productRecipe1)
+                        .productsList(ProductsList.builder()
+                                .id(x.getProductsListId())
+                                .name(x.getProductsListName())
+                                .unit(x.getProductsListUnit())
+                                .build())
+                        .build()).collect(Collectors.toList()) : null);
+
+        RecipesList productRecipe = recipesListDAO.save(productRecipe1);
 
         RecipesListDTO productRecipeDTO = RecipesListDTO.builder()
                 .id(productRecipe.getId())
@@ -97,22 +110,20 @@ public class RecipesListServiceImpl implements RecipesListService {
 
         if (productRecipe.getStepList() != null) {
             productRecipeDTO.setStepList(productRecipe.getStepList().stream()
-                        .map(x->StepListDTO.builder()
-                                .id(x.getId()).stepNr(x.getStepNr()).description(x.getDescription())
-                        .build()).collect(Collectors.toList()));
+                    .map(x -> StepListDTO.builder()
+                            .id(x.getId()).stepNr(x.getStepNr()).description(x.getDescription())
+                            .build()).collect(Collectors.toList()));
         }
 
-//        if(productRecipe.getRecipesListShops() != null){
-//            productRecipeDTO.setRecipesListShop(productRecipe.getRecipesListShops().stream()
-//                    .map(x->RecipesListShopDTO.builder()
-//                            .id(x.getId())
-//                            .build()).collect(Collectors.toList()));
-//        }
 
-        if(productRecipe.getProductsListRecipes() != null){
+        if (productRecipe.getProductsListRecipes() != null) {
             productRecipeDTO.setProductsListRecipe(productRecipe.getProductsListRecipes().stream()
-                    .map(x->ProductsListRecipeDTO.builder()
-                            .id(x.getId()).qty(x.getQty())
+                    .map(x -> ProductsListRecipeDTO.builder()
+                            .id(x.getId())
+                            .qty(x.getQty())
+                            .productsListId(x.getProductsList().getId())
+                            .productsListName(x.getProductsList().getName())
+                            .productsListUnit(x.getProductsList().getUnit())
                             .build()).collect(Collectors.toList()));
         }
 
