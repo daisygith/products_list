f412b483880aa98d127cf59bcc21750637479473
daisygith <daisygith@users.noreply.github.com>
2023-09-07 19:01:00 +0200

    change on CascadeType.MERGE

--- src/main/java/sdu/products_list/entity/ProductsList.java
+++ src/main/java/sdu/products_list/entity/ProductsList.java
@@ -27,7 +27,7 @@ public class ProductsList {
     @Column(name="unit")
     private String unit;
 
-    @OneToMany(mappedBy = "productsList", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "productsList", fetch = FetchType.LAZY, cascade = CascadeType.MERGE)
     private List<ProductsListRecipe> productsListRecipes;
 
     @OneToMany(mappedBy = "productsList", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
