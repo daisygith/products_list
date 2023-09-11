8926c5482696fda73898d1f1fa3ddf801df6aee7
daisygith <daisygith@users.noreply.github.com>
2023-09-11 13:08:37 +0200

    delete some import

--- src/main/java/sdu/products_list/service/ShopListServiceImpl.java
+++ src/main/java/sdu/products_list/service/ShopListServiceImpl.java
@@ -1,11 +1,8 @@
 package sdu.products_list.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.server.ResponseStatusException;
 import sdu.products_list.dao.ShopListDAO;
 import sdu.products_list.dto.ProductsListShopDTO;
 import sdu.products_list.dto.RecipesListDTO;
@@ -16,11 +13,9 @@ import sdu.products_list.entity.RecipesList;
 import sdu.products_list.entity.ShopList;
 import sdu.products_list.exception.ElementNotFoundException;
 
-import java.io.IOException;
+
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
