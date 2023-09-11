48c5cc81ae877a9d62022a4d82a994a7c54e1910
daisygith <daisygith@users.noreply.github.com>
2023-09-11 09:13:57 +0200

    create exception in case fault

--- src/main/java/sdu/products_list/exception/ElementNotFoundException.java
+++ src/main/java/sdu/products_list/exception/ElementNotFoundException.java
@@ -1,12 +1,12 @@
 package sdu.products_list.exception;
 
-public class ElementNotFoundException extends RuntimeException{
+public class ElementNotFoundException extends RuntimeException{ // błąd runtime
 
     public ElementNotFoundException(int id){
-
+        super("Could not find element: " + id);
     }
 
     public ElementNotFoundException (int id, String name){
-        super();
+        super("Could not find element: " + id  + ", group: " + name);//przyjmuje message
     }
 }
