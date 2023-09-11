af8daf8063ba8552f6b290ee198cd7bfb8b74bd5
daisygith <daisygith@users.noreply.github.com>
2023-09-11 09:12:48 +0200

    create java class - ElementNotFoundAdvice is the response when - in case sql couldn't found id.

---
+++ src/main/java/sdu/products_list/exception/ElementNotFoundAdvice.java
@@ -0,0 +1,19 @@
+package sdu.products_list.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class ElementNotFoundAdvice {
+
+    @ResponseBody // odp mogła zostać przemapowana do odpowiedzi json-a
+    @ExceptionHandler(ElementNotFoundException.class) //zawiera klase która bedzie obsługiwana
+    @ResponseStatus(HttpStatus.NOT_FOUND) // status - nie znaleziono wskazanego elementu
+    public String ElementNotFoundHandler(ElementNotFoundException ex){
+        return ex.getMessage();
+    }
+
+}
