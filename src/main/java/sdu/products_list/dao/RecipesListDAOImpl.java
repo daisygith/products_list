1af6013c5eafe59a2b88c97644705a5449e320af
daisygith <daisygith@users.noreply.github.com>
2023-09-14 17:47:07 +0200

    add throws - try- catch

--- src/main/java/sdu/products_list/dao/RecipesListDAOImpl.java
+++ src/main/java/sdu/products_list/dao/RecipesListDAOImpl.java
@@ -5,19 +5,22 @@ import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import sdu.products_list.entity.RecipesList;
+import sdu.products_list.exception.DataBaseException;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class RecipesListDAOImpl implements RecipesListDAO{
+public class RecipesListDAOImpl implements RecipesListDAO {
 
     //define fields for entitymanager
     private EntityManager entityManager;
 
+    int theRecipe;
+
     // set up constructor injuction
     @Autowired
-    public RecipesListDAOImpl(EntityManager entityManager){
+    public RecipesListDAOImpl(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
 
@@ -38,24 +41,47 @@ public class RecipesListDAOImpl implements RecipesListDAO{
     @Override
     public Optional<RecipesList> findById(int theId) {
 
-        // get recipe
-        Optional<RecipesList> theRecipe = Optional.ofNullable(entityManager.find(RecipesList.class, theId));
+        try {
+            // get recipe
+            Optional<RecipesList> theRecipe = Optional.ofNullable(entityManager.find(RecipesList.class, theId));
 
-        return theRecipe;
+            return theRecipe;
+
+        } catch (IllegalArgumentException e) {
+
+            throw new DataBaseException();
+        }
     }
 
     @Override
     public RecipesList save(RecipesList theRecipe) {
 
-        RecipesList dbRecipe = entityManager.merge(theRecipe);
+        try {
+            RecipesList dbRecipe = entityManager.merge(theRecipe);
 
-        return dbRecipe;
+            return dbRecipe;
+        } catch (IllegalArgumentException e) {
+
+            throw new DataBaseException();
+        }
     }
 
     @Override
     public void deleteById(int theId) {
-        RecipesList theRecipe = entityManager.find(RecipesList.class, theId);
 
-        entityManager.remove(theRecipe);
+        try {
+            Optional<RecipesList> theRecipe = Optional.ofNullable(entityManager.find(RecipesList.class, theId));
+
+            if (theRecipe.isPresent()) {
+
+                entityManager.remove(theRecipe.get());
+            }
+        } catch (IllegalArgumentException e) {
+
+            throw new DataBaseException();
+
+        }
     }
 }
+
+
