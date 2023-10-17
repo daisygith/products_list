8e83faf305377a9563610b18e696f00193cddacc
daisygith <daisygith@users.noreply.github.com>
2023-10-17 17:33:00 +0200

    add some library and annotations

--- src/test/java/sdu/products_list/ProductsListApplicationTests.java
+++ src/test/java/sdu/products_list/ProductsListApplicationTests.java
@@ -1,9 +1,16 @@
 package sdu.products_list;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations="classpath:test.properties")
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+@Import(TestConfig.class)
 class ProductsListApplicationTests {
 
 	@Test
