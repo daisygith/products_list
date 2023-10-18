24eac81508dd2c456c9b1f1ce68a7e6846dae2cb
daisygith <daisygith@users.noreply.github.com>
2023-10-18 19:35:04 +0200

    create first test for ShopListDAOImplTest - save all create shop lists

--- src/test/java/sdu/products_list/dao/ShopListDAOImplTest.java
+++ src/test/java/sdu/products_list/dao/ShopListDAOImplTest.java
@@ -1,6 +1,9 @@
 package sdu.products_list.dao;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -8,6 +11,8 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import sdu.products_list.TestConfig;
+import sdu.products_list.entity.ProductsList;
+import sdu.products_list.entity.ShopList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,5 +22,22 @@ import static org.junit.jupiter.api.Assertions.*;
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 @Import(TestConfig.class)
 class ShopListDAOImplTest {
+
+    @Autowired
+    private ShopListDAO shopListDAO;
+
+    @Test
+    public void ShopListDAO_SaveAll_ReturnSaveShopList(){
+
+        ShopList shopList = ShopList.builder()
+                .name("testName_ShopListDAO")
+                .build();
+
+        ShopList saveTest = shopListDAO.save(shopList);
+
+        Assertions.assertNotNull(saveTest);
+        Assertions.assertNotNull(saveTest.getId());
+
+    }
 
 }
