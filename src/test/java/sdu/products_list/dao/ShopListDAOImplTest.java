5b14081fa3fe44ac7dd806a1449461b0197e5d62
daisygith <daisygith@users.noreply.github.com>
2023-10-18 19:30:59 +0200

    add implementation to ShopListDAOImplTest

--- src/test/java/sdu/products_list/dao/ShopListDAOImplTest.java
+++ src/test/java/sdu/products_list/dao/ShopListDAOImplTest.java
@@ -1,7 +1,21 @@
 package sdu.products_list.dao;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import sdu.products_list.TestConfig;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@TestPropertySource(locations="classpath:test.properties")
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+@Import(TestConfig.class)
 class ShopListDAOImplTest {
 
 }
