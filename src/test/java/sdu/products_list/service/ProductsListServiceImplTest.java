e3287673022dfe3dc5a95993edda83c8fc9f4191
daisygith <daisygith@users.noreply.github.com>
2023-10-24 19:41:20 +0200

    created test for ProductsListService

--- src/test/java/sdu/products_list/service/ProductsListServiceImplTest.java
+++ src/test/java/sdu/products_list/service/ProductsListServiceImplTest.java
@@ -1,25 +1,42 @@
 package sdu.products_list.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import sdu.products_list.dao.ProductsListDAO;
 import sdu.products_list.dto.ProductsListDTO;
 import sdu.products_list.entity.ProductsList;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class ProductsListServiceImplTest {
 
+    @Mock
+    private ProductsListDAO productsListDAO;
+
+    @InjectMocks
+    private ProductsListServiceImpl productsListService;
+
     @Test
-    void findById() {
-        var findID = new ProductsListDTO();
-        assertNotNull(findID.getId());
+    public void ProductsList_CreateProduct_ReturnProductsListDTO(){
+
+        ProductsList productsList = ProductsList.builder()
+                .name("testName_ProductListDTO")
+                .unit("testUnit_ProductListDTO")
+                .build();
+
+        ProductsListDTO productsListDTO = ProductsListDTO.builder()
+                .name("testName_ProductListDTO")
+                .unit("testUnit_ProductListDTO")
+                .build();
+
+        when(productsListDAO.save(Mockito.any(ProductsList.class))).thenReturn(productsList);
 
     }
 
-    @Test
-    void save() {
-    }
-
-    @Test
-    void deleteById() {
-    }
 }
