502bd99cb22ac582863267652c00015c3fa3ac8f
daisygith <daisygith@users.noreply.github.com>
2023-10-25 19:56:29 +0200

    created test for ProductsListService - findAllProducts

--- src/test/java/sdu/products_list/service/ProductsListServiceImplTest.java
+++ src/test/java/sdu/products_list/service/ProductsListServiceImplTest.java
@@ -1,14 +1,18 @@
 package sdu.products_list.service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
 import sdu.products_list.dao.ProductsListDAO;
 import sdu.products_list.dto.ProductsListDTO;
 import sdu.products_list.entity.ProductsList;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
@@ -23,19 +27,16 @@ class ProductsListServiceImplTest {
     private ProductsListServiceImpl productsListService;
 
     @Test
-    public void ProductsList_CreateProduct_ReturnProductsListDTO(){
+    public void ProductsListService_findAllProducts_ReturnsResponseDTO(){
 
-        ProductsList productsList = ProductsList.builder()
-                .name("testName_ProductListDTO")
-                .unit("testUnit_ProductListDTO")
-                .build();
+        List<ProductsList> productsList = Mockito.mock(List.class);
 
-        ProductsListDTO productsListDTO = ProductsListDTO.builder()
-                .name("testName_ProductListDTO")
-                .unit("testUnit_ProductListDTO")
-                .build();
+        when(productsListDAO.findAllProducts()).thenReturn(productsList);
 
-        when(productsListDAO.save(Mockito.any(ProductsList.class))).thenReturn(productsList);
+        List<ProductsListDTO> saveProductsList = productsListService.findAllProducts();
+
+        Assertions.assertNotNull(saveProductsList);
+
 
     }
 
