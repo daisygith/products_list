2e2bb31be5b301452ba795024f3b10ebfaaf3f8b
daisygith <daisy.gith@gmail.com>
2023-10-30 08:19:49 +0100

    create RecipesListServiceImplTest

---
+++ src/test/java/sdu/products_list/service/RecipesListServiceImplTest.java
@@ -0,0 +1,26 @@
+package sdu.products_list.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(MockitoExtension.class)
+class RecipesListServiceImplTest {
+
+    @Test
+    void RecipesListService_findAll_ReturnRecipesList() {
+    }
+
+    @Test
+    void RecipesListService_findById_ReturnRecipesList() {
+    }
+
+    @Test
+    void RecipesListService_save_ReturnRecipesList() {
+    }
+
+    @Test
+    void RecipesListService_deleteById() {
+    }
+}
