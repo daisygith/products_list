d48577e501e2ad3c454ba9b9707ca35bedb8d083
daisygith <daisy.gith@gmail.com>
2023-10-30 19:50:44 +0100

    create test for ProductsListRestController - addProducts

---
+++ src/test/java/sdu/products_list/rest/ProductsListRestControllerTest.java
@@ -0,0 +1,93 @@
+package sdu.products_list.rest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatcher;
+import org.mockito.ArgumentMatchers;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import sdu.products_list.dto.ProductsListDTO;
+import sdu.products_list.entity.ProductsList;
+import sdu.products_list.service.ProductsListService;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+
+@WebMvcTest(controllers =  ProductsListRestController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
+class ProductsListRestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProductsListService productsListService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private ProductsList productsList;
+    private ProductsListDTO productsListDTO;
+
+    @BeforeEach
+    public void init(){
+         productsList = ProductsList.builder()
+                .name("testName_ProductListDAO")
+                .unit("testUnit_ProductListDAO")
+                .productsListRecipes(new ArrayList<>())
+                .productsListShop(new ArrayList<>())
+                .build();
+
+        productsListDTO = ProductsListDTO.builder()
+                .name("testName_ProductListDAO")
+                .unit("testUnit_ProductListDAO")
+                .productsListRecipe(new ArrayList<>())
+                .productsListShop(new ArrayList<>())
+                .build();
+    }
+
+    @Test
+    void findAllProducts() {
+    }
+
+    @Test
+    void getProductsList() {
+    }
+
+    @Test
+    public void  ProductsListRestController_addProducts_ReturnCreated() throws Exception {
+
+        given(productsListService.save(ArgumentMatchers.any()))
+                .willAnswer((invocation -> invocation.getArgument(0)));
+
+        ResultActions resultActions = mockMvc.perform(post("/api/productsList")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(productsListDTO)));
+
+        resultActions.andExpect(MockMvcResultMatchers.status().isCreated());
+
+
+    }
+
+    @Test
+    void updateProducts() {
+    }
+
+    @Test
+    void deleteProducts() {
+    }
+}
