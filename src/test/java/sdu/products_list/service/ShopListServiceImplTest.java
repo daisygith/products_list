3fe81ad4c98ec053b0189a882d986ed6c77a76fb
daisygith <daisy.gith@gmail.com>
2023-10-30 17:53:34 +0100

    create test for ShopServiceImpl: -findAll() -findById() -save() -deleteById()

--- src/test/java/sdu/products_list/service/ShopListServiceImplTest.java
+++ src/test/java/sdu/products_list/service/ShopListServiceImplTest.java
@@ -1,13 +1,24 @@
 package sdu.products_list.service;
 
+import com.sun.source.tree.ModuleTree;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import sdu.products_list.dao.ShopListDAO;
+import sdu.products_list.dto.ShopListDTO;
+import sdu.products_list.entity.ShopList;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 class ShopListServiceImplTest {
     @Mock
@@ -17,18 +28,64 @@ class ShopListServiceImplTest {
     private ShopListServiceImpl shopListService;
 
     @Test
-    void ShopListService_findAll() {
+    public void ShopListService_findAll_ReturnShopList() {
+
+        List<ShopList> shopList = Mockito.mock(List.class);
+        when(shopListDAO.findAllShopList()).thenReturn(shopList);
+
+        List<ShopListDTO> saveShopList = shopListService.findAll();
+        Assertions.assertNotNull(saveShopList);
+
     }
 
     @Test
-    void findById() {
+    public void ShopListService_findById_ReturnShopList() throws Exception{
+        ShopList shopList = ShopList.builder()
+                .id(1)
+                .name("testName_ShopListDAO")
+                .recipesList(new ArrayList<>())
+                .productsListShop(new ArrayList<>())
+                .build();
+
+        when(shopListDAO.findById(1)).thenReturn(shopList);
+
+        ShopListDTO saveShopList = shopListService.findById(1);
+        Assertions.assertNotNull(saveShopList);
+
     }
 
     @Test
-    void save() {
+    public void ShopListService_save_ReturnShopList() {
+        ShopList shopList = ShopList.builder()
+                .id(1)
+                .name("testName_ShopListDAO")
+                .recipesList(new ArrayList<>())
+                .productsListShop(new ArrayList<>())
+                .build();
+
+        ShopListDTO shopListDTO = ShopListDTO.builder()
+                .id(1)
+                .name("testName_ShopListDTO")
+                .recipesList(new ArrayList<>())
+                .productsListShop(new ArrayList<>())
+                .build();
+
+        when(shopListDAO.save(Mockito.any(ShopList.class))).thenReturn(shopList);
+
+        ShopListDTO saveShopList = shopListService.save(shopListDTO);
+
+        Assertions.assertNotNull(saveShopList);
     }
 
     @Test
-    void deleteById() {
+    public void ShopListService_deleteById() {
+        ShopList shopList = ShopList.builder()
+                .id(1)
+                .name("testName_ShopListDAO")
+                .recipesList(new ArrayList<>())
+                .productsListShop(new ArrayList<>())
+                .build();
+
+        assertAll(() -> shopListService.deleteById(1));
     }
 }
