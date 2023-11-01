654ca355751c5f7a8df30987dfaecb8d86c40ea3
daisygith <daisy.gith@gmail.com>
2023-11-01 19:44:54 +0100

    create test for  RecipesListRestController_findAll_ReturnAll() ->     @GetMapping("/shoplist")

---
+++ src/test/java/sdu/products_list/rest/ShopListRestControllerTest.java
@@ -0,0 +1,93 @@
+package sdu.products_list.rest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import sdu.products_list.dto.ShopListDTO;
+import sdu.products_list.entity.ShopList;
+import sdu.products_list.service.ShopListService;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+@WebMvcTest(controllers =  ShopListRestController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
+class ShopListRestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ShopListService shopListService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private ShopList shopList;
+    private ShopListDTO shopListDTO;
+
+    @BeforeEach
+    public void init(){
+        shopList = ShopList.builder()
+                .name("testNAme_ShopListDAO")
+                .recipesList(new ArrayList<>())
+                .productsListShop(new ArrayList<>())
+                .build();
+
+        shopListDTO = ShopListDTO.builder()
+                .name("testName_ShopListDTO")
+                .recipesList(new ArrayList<>())
+                .shopList(new ArrayList<>())
+                .productsListShop(new ArrayList<>())
+                .build();
+    }
+
+    @Test
+    public void RecipesListRestController_findAll_ReturnAll() throws Exception{
+
+        List<ShopListDTO> returnShopList = Arrays.asList(shopListDTO);
+
+        Mockito.when(shopListService.findAll()).thenReturn(returnShopList);
+
+        ResultActions resultActions = mockMvc.perform(get("/shop/shoplist")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        resultActions.andExpect(MockMvcResultMatchers.status().isOk());
+
+    }
+
+    @Test
+    void getShopList() {
+    }
+
+    @Test
+    void addShopList() {
+    }
+
+    @Test
+    void updateShopList() {
+    }
+
+    @Test
+    void deleteShopList() {
+    }
+}
